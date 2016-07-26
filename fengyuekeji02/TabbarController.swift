4f586cc52649b34cafb2d6e18f1fe90c26b92fb6
yuzhouheike <26108724@qq.com>
2016-07-26 08:50:35 +0800

    swift框架基础

--- fengyuekeji02/TabbarController.swift
+++ fengyuekeji02/TabbarController.swift
@@ -14,76 +14,53 @@ class TabbarController: UITabBarController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        UINavigationBar.appearance().barTintColor = #colorLiteral(red: 0, green: 0.501960814, blue: 1, alpha: 1)
+        UITabBar.appearance().backgroundColor = UIColor.yellow()
+//        UITabBar.appearance().tintColor = UIColor.red()//
         
         
         let firstVC = FirstController();
         let nav1 = UINavigationController(rootViewController:firstVC);
-        let image1 = UIImage(named:"首页");
+        let image1 = UIImage.init(named:"首页选中")
+        
+        nav1.tabBarItem.selectedImage = UIImage.init(named: "首页")
         nav1.tabBarItem = UITabBarItem(title:"首页",image:image1,tag:1);
         
         
         
         let secondVC = SecondController();
         let nav2 = UINavigationController(rootViewController:secondVC)
-        let image2 = UIImage(named: "分类")
+        let image2 = UIImage(named: "分类选中")
+        nav2.tabBarItem.selectedImage = UIImage.init(named: "分类")
         nav2.tabBarItem = UITabBarItem(title:"分类",image:image2, tag:2)
         
         
         
         let thirdVC = ThirdController();
         let nav3 = UINavigationController(rootViewController:thirdVC)
-        let image3 = UIImage(named: "晒单区")
+        let image3 = UIImage(named: "晒单区选中")
+        nav3.tabBarItem.selectedImage = UIImage.init(named: "晒单区")
         nav3.tabBarItem = UITabBarItem(title:"晒单区",image:image3, tag:3)
         
         let fourthVC = FourthController();
         let nav4 = UINavigationController(rootViewController:fourthVC)
-        let image4 = UIImage.init(named: "购物车")
+        
+        let image4 = UIImage.init(named: "购物车选中")
+        nav4.tabBarItem.selectedImage = UIImage.init(named: "购物车")
         nav4.tabBarItem = UITabBarItem.init(title: "购物车", image: image4, tag: 4)
         
         let fifthVC = FifthController();
         let nav5 = UINavigationController.init(rootViewController: fifthVC)
-        let image5 = UIImage.init(named: "个人中心")
+        let image5 = UIImage.init(named: "个人中心选中")
+        nav5.tabBarItem.selectedImage = UIImage.init(named: "个人中心")
         nav5.tabBarItem = UITabBarItem.init(title: "个人中心", image: image5, tag: 5)
         
         let navarray = [nav1, nav2, nav3, nav4, nav5]
-        
-        
-        
-        
+     
         
         self.viewControllers = navarray;
         
-        
-        /**
-    
-        
-         let firstVC=FirstController();
-         let nav1=UINavigationController(rootViewController:firstVC);
-         let image1=UIImage(named:"h.png");
-         nav1.tabBarItem=UITabBarItem(title:"完美食材",image:image1,tag:1);
-         
-         let secondVC=SecondConreoller();
-         let nav2=UINavigationController(rootViewController:secondVC);
-         let image2=UIImage(named:"o.png");
-         nav2.tabBarItem=UITabBarItem(title:"国色天香",image:image2,tag:2);
-         
-         let thirdVC=ThirdController();
-         let nav3=UINavigationController(rootViewController:thirdVC);
-         let image3=UIImage(named:"s.png");
-         nav3.tabBarItem=UITabBarItem(title:"实惠经典",image:image3,tag:3);
-         
-         let navArr=[nav1,nav2,nav3];
-         let tabBarController=UITabBarController();
-         tabBarController.viewControllers=navArr;
-         self.window!.rootViewController=tabBarController;
-         
-         self.window!.makeKeyAndVisible()
-         return true
-         }
 
-         
-         */
-        
         
 //        self.tabBarItems = array
         
